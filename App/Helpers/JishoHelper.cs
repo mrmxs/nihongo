a48048b469086c8b1183b2df6a6dfb375bd8ca34
mrmxs <maryzmaxs@gmail.com>
2022-10-23 23:35:52 +0300

    Jisho Helper. Add model. Add helper stub

---
+++ App/Helpers/JishoHelper.cs
@@ -0,0 +1,20 @@
+namespace Nihongo.App.Helpers;
+
+public class JishoHelper
+{
+    // https://jisho.org/search/劇获画%23kanji
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
---
+++ Core/Models/JishoKanji.cs
@@ -0,0 +1,13 @@
+namespace Nihongo.Core.Models;
+
+public class JishoKanji
+{
+    public string Kanji { get; set; }
+    public string Meaning { get; set; }
+    public string KunReading { get; set; }
+    public string OnReading { get; set; }
+    public string Grade { get; set; }
+    public string JLPT { get; set; }
+    public string Frequency { get; set; }
+    public string StrokeOrder { get; set; }
+}
