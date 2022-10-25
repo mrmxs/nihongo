cd03fb7c7142fd48c3db3c9f245a3af1f4025b66
mrmxs <maryzmaxs@gmail.com>
2022-10-25 03:19:35 +0300

    move get methods to GetHelper

--- Console/Program.cs
+++ Console/Program.cs
@@ -8,38 +8,23 @@ using System.Text;
 
 public class Program
 {
-    private static string _testFile = @"test\鬼劇获画 #kanji - Jisho.org.htm";
-    private static string _testUrl = "https://jisho.org/search/劇获画%23kanji";
+    private readonly static string _testFile =
+        $@"{Environment.CurrentDirectory}\test\鬼劇获画 #kanji - Jisho.org.htm";
+    private readonly static string _testUrl = "https://jisho.org/search/劇获画%23kanji";
 
     public static void Main(string[] args)
     {
-        TestJishoHelper(GetFromFile());
+        TestJishoHelper();
     }
 
-    private static void TestJishoHelper(string htmlSrc) {
+    private static void TestJishoHelper() {
+        // var htmlSrc = GetHelper.FromFile(_testFile);
+        var htmlSrc = GetHelper.FromUrl(_testUrl);
+
         var jisho = new JishoHelper(htmlSrc);
 
         foreach (var kanji in jisho.Kanjis)
             Console.WriteLine(kanji);
-    }
-
-    private static string GetFromUrl() {
-        using (HttpClient client = new HttpClient())
-        {
-            using (HttpResponseMessage response = client.GetAsync(_testUrl).Result)
-            {
-                using (HttpContent content = response.Content)
-                {
-                    return content.ReadAsStringAsync().Result;
-                }
-            }
-        }
-    }
-
-    private static string GetFromFile() {
-        var path = Environment.CurrentDirectory + $@"\{_testFile}";
-        
-        return File.ReadAllText(path);
     }
 }
 
